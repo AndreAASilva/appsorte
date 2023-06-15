07d15612f0246df725db0687841fc1852740e26e
André Anderson Alves Silva <104483772+AndreAASilva@users.noreply.github.com>
2023-06-15 10:28:35 -0300

    Projeto Biscoito da Sorte
    Projeto criado em aula de React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import './estilo.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      textoFrase:''
+    }
+
+    this.quebraBiscoito = this.quebraBiscoito.bind(this)
+
+    this.frases = ['Siga os bons e aprenda com eles.', 'O riso é a menor distância entre duas pessoas.','Deixe de lado as preocupações e seja feliz.', 'Realize o óbvio, pense no improvável e conquiste o impossível.','Acredite em milagres, mas não dependa deles.', 'A maior barreira para o sucesso é o medo do fracasso.' ]
+  }
+
+  quebraBiscoito(){
+    let state = this.state;
+    let numeroAleatorio = Math.floor(Math.random() * this.frases.length)
+    state.textoFrase = '" '+this.frases[numeroAleatorio]+' "'
+    this.setState(state)
+  }
+
+  render(){
+    return(
+      <div className='container'>
+        <img src={require('./assets/biscoito.png')} className='img'/>
+        <Botao nome='Abrir Biscoito' acaoBtn={this.quebraBiscoito}/>
+        <h3 className='textoFrase'>{this.state.textoFrase}</h3>
+      </div>
+    )
+  }
+}
+
+class Botao extends Component{
+
+  render(){
+    return(
+      <div>
+        <button onClick={this.props.acaoBtn}>{this.props.nome}</button>
+      </div>
+    )
+  }
 }
 
 export default App;
