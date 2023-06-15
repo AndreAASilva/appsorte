f6b9f151265774a21d478269ea2425765d87b6c7
André Anderson Alves Silva <104483772+AndreAASilva@users.noreply.github.com>
2023-06-15 11:19:49 -0300

    Finalizado
    Projeto finalizado

--- src/App.js
+++ src/App.js
@@ -11,19 +11,22 @@ class App extends Component{
 
     this.quebraBiscoito = this.quebraBiscoito.bind(this)
 
+    
     this.frases = ['Siga os bons e aprenda com eles.', 'O riso é a menor distância entre duas pessoas.','Deixe de lado as preocupações e seja feliz.', 'Realize o óbvio, pense no improvável e conquiste o impossível.','Acredite em milagres, mas não dependa deles.', 'A maior barreira para o sucesso é o medo do fracasso.' ]
   }
 
+  
   quebraBiscoito(){
-    let state = this.state;
-    let numeroAleatorio = Math.floor(Math.random() * this.frases.length)
-    state.textoFrase = '" '+this.frases[numeroAleatorio]+' "'
-    this.setState(state)
+    let state = this.state; 
+    let numeroAleatorio = Math.floor(Math.random() * this.frases.length) 
+    state.textoFrase = '" '+this.frases[numeroAleatorio]+' "' 
+    this.setState(state) 
   }
 
   render(){
     return(
       <div className='container'>
+        
         <img src={require('./assets/biscoito.png')} className='img'/>
         <Botao nome='Abrir Biscoito' acaoBtn={this.quebraBiscoito}/>
         <h3 className='textoFrase'>{this.state.textoFrase}</h3>
@@ -31,6 +34,7 @@ class App extends Component{
     )
   }
 }
+
 
 class Botao extends Component{
 
